2b619a71af248de3bca693cb193a3afcc8b2fd0d
Skylarr <theotherskylarr@gmail.com>
2024-02-11 05:10:20 -0800

    Work on Badge standardization

--- scripts/prefabs/badge_fasthatter.lua
+++ scripts/prefabs/badge_fasthatter.lua
@@ -77,9 +77,15 @@ local function fn()
 
 	inst:AddComponent("inspectable")
 
+	inst:AddComponent("equippable")
+	inst.components.equippable.restrictedtag = "badgerestricted"
+	inst.components.equippable.equipslot = EQUIPSLOTS.BADGE1
+    -- inst.components.equippable:SetOnEquip( OnEquip )
+    -- inst.components.equippable:SetOnUnequip( OnUnequip )
+
 	inst:AddComponent("badge")
-	inst.components.badge:SetOnEquip(OnEquip)
-	inst.components.badge:SetOnUnequip(OnUnequip)
+	-- inst.components.badge:SetOnEquip(OnEquip)
+	-- inst.components.badge:SetOnUnequip(OnUnequip)
 
 	inst:AddComponent("fueled")
 	inst.components.fueled.fueltype = FUELTYPE.MAGIC
--- scripts/prefabs/badge_football.lua
+++ scripts/prefabs/badge_football.lua
@@ -8,29 +8,6 @@ RegisterInventoryItemAtlas("images/inventoryimages/badge_football.xml","badge_fo
 
 STRINGS.NAMES.BADGE_FOOTBALL = "Wood Armor Badge"
 
-local function OnEmpty(inst)
-    if inst.components.inventoryitem ~= nil and inst.components.inventoryitem:GetGrandOwner() ~= nil then
-        inst.components.inventoryitem:GetGrandOwner():PushEvent("toolbroke", { tool = inst })
-    end
-
-    -- inst:Remove()
-end
-
-local function OnEquip(inst, owner)
-	local hatpack = inst.components.badge.hatpack
-	hatpack:AddComponent("armor")
-	hatpack.components.armor:InitIndestructible(TUNING.ARMORWOOD_ABSORPTION)
-	-- hatpack.components.armor:SetCondition(inst.components.armor.condition)
-	hatpack.components.armor.ontakedamage = function(pack, damage)
-		inst.components.armor:TakeDamage(damage)
-	end
-end
-
-local function OnUnequip(inst, owner)
-	local hatpack = inst.components.badge.hatpack
-	hatpack:RemoveComponent("armor") -- this will break if we have multiple armor badges
-end
-
 local function fn() 
     local inst = CreateEntity()
 
@@ -72,20 +49,14 @@ local function fn()
 
 	inst:AddComponent("inspectable")
 
+	inst:AddComponent("equippable")
+	inst.components.equippable.restrictedtag = "badgerestricted"
+	inst.components.equippable.equipslot = EQUIPSLOTS.BADGE1
+
 	inst:AddComponent("badge")
-	inst.components.badge:SetOnEquip(OnEquip)
-	inst.components.badge:SetOnUnequip(OnUnequip)
 
 	inst:AddComponent("armor")
-    inst.components.armor:InitCondition(TUNING.ARMORWOOD, TUNING.ARMORWOOD_ABSORPTION)
-	inst.components.armor:SetOnFinished(OnEmpty)
-    -- inst.components.armor:AddWeakness("beaver", TUNING.BEAVER_WOOD_DAMAGE)
-
-	-- inst:AddComponent("fueled")
-	-- inst.components.fueled.fueltype = FUELTYPE.MAGIC
-	-- inst.components.fueled:InitializeFuelLevel(50)
-	-- inst.components.fueled:SetDepletedFn(OnEmpty)
-
+    inst.components.armor:InitCondition(TUNING.ARMOR_FOOTBALLHAT, TUNING.ARMOR_FOOTBALLHAT_ABSORPTION)
     return inst
 end
 
